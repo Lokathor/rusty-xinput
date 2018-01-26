7dcba2fdf3406ca12c1f13b0a38009a4d481c8ce
Lokathor <zefria@gmail.com>
2018-01-25 23:31:11 -0700

    Dynamic loading is now thread safe.

--- examples/demo.rs
+++ examples/demo.rs
@@ -19,6 +19,12 @@ type XInputSetStateFunc = unsafe extern "system" fn(DWORD, *mut XINPUT_VIBRATION
 static mut global_xinput_handle: HMODULE = ::std::ptr::null_mut();
 static mut opt_xinput_get_state: Option<XInputGetStateFunc> = None;
 static mut opt_xinput_set_state: Option<XInputSetStateFunc> = None;
+static xinput_status: ::std::sync::atomic::AtomicUsize = ::std::sync::atomic::ATOMIC_USIZE_INIT;
+const ordering: ::std::sync::atomic::Ordering = ::std::sync::atomic::Ordering::SeqCst;
+
+const xinput_UNINITIALIZED: usize = 0;
+const xinput_LOADING: usize = 1;
+const xinput_ACTIVE: usize = 2;
 
 fn wide_null<S: AsRef<str>>(s: S) -> Vec<u16> {
   let mut output = vec![];
@@ -38,52 +44,76 @@ fn show_wide_null(arr: &[u16]) -> String {
 }
 
 unsafe fn dynamic_load_xinput() {
-  let xinput14 = wide_null("xinput1_4.dll");
-  let xinput91 = wide_null("xinput9_1_0.dll");
-  let xinput13 = wide_null("xinput1_3.dll");
-
-  let mut xinput_handle: HMODULE = ::std::ptr::null_mut();
-  for lib_name in vec![xinput14, xinput91, xinput13] {
-    trace!(
-      "Attempting to load XInput DLL: {}",
-      show_wide_null(&lib_name)
-    );
-    xinput_handle = LoadLibraryW(lib_name.as_ptr());
-    if !xinput_handle.is_null() {
-      debug!("Success: XInput Loaded: {}", show_wide_null(&lib_name));
-      break;
+  // The result status is if the value was what we expected, and the value
+  // inside is actual value seen.
+  match xinput_status.compare_exchange(xinput_UNINITIALIZED, xinput_LOADING, ordering, ordering) {
+    Err(xinput_LOADING) => {
+      debug!("A call to 'dynamic_load_xinput' was made while XInput was already loading.");
     }
-  }
-  if xinput_handle.is_null() {
-    debug!("Failure: XInput could not be loaded.");
-  } else {
-    let get_state_name = CString::new("XInputGetState").unwrap();
-    let set_state_name = CString::new("XInputSetState").unwrap();
-
-    let get_state_ptr = GetProcAddress(xinput_handle, get_state_name.as_ptr());
-    if !get_state_ptr.is_null() {
-      trace!("Found function {:?}.", get_state_name);
-      opt_xinput_get_state = Some(::std::mem::transmute(get_state_ptr));
-    } else {
-      trace!("Could not find function {:?}.", get_state_name);
+    Err(xinput_ACTIVE) => {
+      debug!("A call to 'dynamic_load_xinput' was made while XInput was already active.");
     }
-
-    let set_state_ptr = GetProcAddress(xinput_handle, set_state_name.as_ptr());
-    if !set_state_ptr.is_null() {
-      trace!("Found Function {:?}.", set_state_name);
-      opt_xinput_set_state = Some(::std::mem::transmute(set_state_ptr));
-    } else {
-      trace!("Could not find function {:?}.", set_state_name);
+    Err(_) => {
+      warn!("A call to 'dynamic_load_xinput' was made while XInput was in an unknown state.");
     }
+    Ok(_) => {
+      let xinput14 = wide_null("xinput1_4.dll");
+      let xinput91 = wide_null("xinput9_1_0.dll");
+      let xinput13 = wide_null("xinput1_3.dll");
 
-    if opt_xinput_get_state.is_some() && opt_xinput_set_state.is_some() {
-      global_xinput_handle = xinput_handle;
-      debug!("Function pointers loaded successfully.");
-    } else {
-      opt_xinput_get_state = None;
-      opt_xinput_set_state = None;
-      FreeLibrary(xinput_handle);
-      debug!("Could not load the function pointers.");
+      let mut xinput_handle: HMODULE = ::std::ptr::null_mut();
+      for lib_name in vec![xinput14, xinput91, xinput13] {
+        trace!(
+          "Attempting to load XInput DLL: {}",
+          show_wide_null(&lib_name)
+        );
+        xinput_handle = LoadLibraryW(lib_name.as_ptr());
+        if !xinput_handle.is_null() {
+          debug!("Success: XInput Loaded: {}", show_wide_null(&lib_name));
+          break;
+        }
+      }
+      if xinput_handle.is_null() {
+        debug!("Failure: XInput could not be loaded.");
+        xinput_status
+          .compare_exchange(xinput_LOADING, xinput_UNINITIALIZED, ordering, ordering)
+          .ok();
+      } else {
+        let get_state_name = CString::new("XInputGetState").unwrap();
+        let set_state_name = CString::new("XInputSetState").unwrap();
+
+        let get_state_ptr = GetProcAddress(xinput_handle, get_state_name.as_ptr());
+        if !get_state_ptr.is_null() {
+          trace!("Found function {:?}.", get_state_name);
+          opt_xinput_get_state = Some(::std::mem::transmute(get_state_ptr));
+        } else {
+          trace!("Could not find function {:?}.", get_state_name);
+        }
+
+        let set_state_ptr = GetProcAddress(xinput_handle, set_state_name.as_ptr());
+        if !set_state_ptr.is_null() {
+          trace!("Found Function {:?}.", set_state_name);
+          opt_xinput_set_state = Some(::std::mem::transmute(set_state_ptr));
+        } else {
+          trace!("Could not find function {:?}.", set_state_name);
+        }
+
+        if opt_xinput_get_state.is_some() && opt_xinput_set_state.is_some() {
+          global_xinput_handle = xinput_handle;
+          debug!("Function pointers loaded successfully.");
+          xinput_status
+            .compare_exchange(xinput_LOADING, xinput_ACTIVE, ordering, ordering)
+            .ok();
+        } else {
+          opt_xinput_get_state = None;
+          opt_xinput_set_state = None;
+          FreeLibrary(xinput_handle);
+          debug!("Could not load the function pointers.");
+          xinput_status
+            .compare_exchange(xinput_LOADING, xinput_UNINITIALIZED, ordering, ordering)
+            .ok();
+        }
+      }
     }
   }
 }
@@ -92,8 +122,12 @@ extern crate simple_logger;
 
 fn main() {
   simple_logger::init().unwrap();
+
+  trace!("{:?}", xinput_status.load(ordering));
+
   unsafe {
     dynamic_load_xinput();
+    trace!("{:?}", xinput_status.load(ordering));
 
     gamepad();
   }
